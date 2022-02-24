ff75cd8a7c67262b43bc8f9fe18ac5be4c30c62a
SergeyDedikov <serded59@yandex.ru>
2022-02-24 21:17:27 +0500

    feat: add celebrate in users routes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,18 @@
 const router = require("express").Router();
+const { celebrate, Joi } = require("celebrate");
+const { isEmail } = require("validator");
 
 const { getUser, updateUser } = require("../controllers/users");
 
-router.get("/users/me", getUser);
-router.patch("/users/me", updateUser);
+const validateCelebrate = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().custom(isEmail),
+  }),
+});
+
+router.get("/users/me", validateCelebrate(), getUser);
+
+router.patch("/users/me", validateCelebrate(), updateUser);
 
 module.exports = router;
