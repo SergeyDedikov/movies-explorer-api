fac96f9bc7fc74a1b887c1cc3fcc278a6192e5e3
SergeyDedikov <serded59@yandex.ru>
2022-02-25 18:21:18 +0500

    feat: change type error

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,13 +1,14 @@
 const jwt = require("jsonwebtoken");
 
 const { NODE_ENV, JWT_SECRET } = process.env;
+const Unauthorized = require("../errors/unauthorized-error");
 
 const auth = (req, res, next) => {
   // извлекаем токен из куков запроса
   const token = req.cookies.jwt;
 
   if (!token) {
-    return next(new Error("Необходима авторизация"));
+    return next(new Unauthorized("Необходима авторизация"));
   }
 
   let payload;
@@ -19,7 +20,7 @@ const auth = (req, res, next) => {
       NODE_ENV === "production" ? JWT_SECRET : "secret-string"
     );
   } catch (err) {
-    return next(new Error("Некорректный токен"));
+    return next(new Unauthorized("Некорректный токен"));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
