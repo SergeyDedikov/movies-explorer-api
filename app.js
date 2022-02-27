ac389869b965d96cd91d87e4fa5bb1a612ac83a3
SergeyDedikov <serded59@yandex.ru>
2022-02-27 18:27:25 +0500

    refactor: routes

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const { celebrate, Joi, errors } = require("celebrate");
+const { errors } = require("celebrate");
 
-const NotFoundError = require("./errors/not-found-error");
 const errorHandler = require("./middlewares/error-handler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 const router = require("./routes");
-const { createUser, login, signout } = require("./controllers/users");
 
 const app = express();
 const { PORT = 3000, DB_PATH = `mongodb://localhost:27017/bitfilmsdb` } =
@@ -25,38 +23,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(requestLogger); // логгер запросов
 
-// -- Маршруты авторизации
-app.post(
-  "/signup",
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  createUser
-);
-
-app.post(
-  "/signin",
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  login
-);
-
-app.post("/signout", signout);
-
-// -- Защищённые маршруты
 app.use(router);
-
-app.use((req, res, next) => {
-  next(new NotFoundError("Был запрошен несуществующий роут"));
-});
 
 app.use(errorLogger); // логгер ошибок
 app.use(errors()); // обработчик ошибок celebrate
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,25 @@ const router = require("express").Router();
 const usersRoutes = require("./users");
 const moviesRoutes = require("./movies");
 const auth = require("../middlewares/auth");
+const { createUser, login, signout } = require("../controllers/users");
+const {
+  validateBodyCreateUser,
+  validateAuthentication,
+} = require("../middlewares/validations");
+const NotFoundError = require("../errors/not-found-error");
 
+// -- Маршруты авторизации
+router.post("/signup", validateBodyCreateUser, createUser);
+router.post("/signin", validateAuthentication, login);
+router.post("/signout", signout);
+
+// -- Защищённые маршруты
 router.use(auth);
 router.use(usersRoutes);
 router.use(moviesRoutes);
+
+router.use((req, res, next) => {
+  next(new NotFoundError("Был запрошен несуществующий роут"));
+});
 
 module.exports = router;
